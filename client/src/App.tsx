e0381a6b2ba884c6a1a3256790a4bcdb77b28016
Md-Mahir-Asef <mdmahirasef2008@gmail.com>
2025-08-07 15:55:58 +0600

    Frontend Init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,12 @@
+import { Analytics } from "@vercel/analytics/next";
+
 function App() {
-  return <h1 className="text-9xl text-amber-950">Hello World</h1>;
+  return (
+    <>
+      <h1 className="text-9xl text-amber-950">Hello World</h1>
+      <Analytics />
+    </>
+  );
 }
 
 export default App;
