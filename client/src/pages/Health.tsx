171306d6d10a7388ba66eb7bd7399dd0b12da2bf
Md-Mahir-Asef <mdmahirasef2008@gmail.com>
2025-08-28 12:05:41 +0600

    feat(client[/health]): Add health details in client.

--- client/src/pages/Health.tsx
+++ client/src/pages/Health.tsx
@@ -4,28 +4,51 @@ import axios from "axios";
 import { config } from "../config/config";
 
 export default function Health() {
-    const [text, setText] = useState("");
+    const [data, setData] = useState<{
+        uptime: string;
+        timeStamp: string;
+        availability: {
+            database: {
+                size: string;
+                available: boolean;
+            };
+        };
+    }>();
     useEffect(() => {
         const fetchData = async () => {
-            const data = await axios.get(
+            const response = await axios.get(
                 `${config.VITE_SERVER_BASE_URL}/health`,
                 {
                     withCredentials: true,
                 }
             );
-            console.log("Data: ", data.data.message);
-            setText(data.data.message);
+            setData(response.data.data);
         };
         fetchData();
     }, []);
     return (
         <>
             <Header />
-            <div>
-                <h1 className="text-8xl text-red-800 w-screen mx-auto">
-                    {text ? text : "Not connected to server."}
+            <div className="flex flex-col items-center mt-20">
+                <h1 className="text-8xl text-red-800">
+                    {data ? "Connect to Server." : "Not connected to server."}
                 </h1>
-                <h2>Docker Compose Running</h2>
+                {data ? (
+                    <div className="flex flex-row justify-center mt-20 gap-30">
+                        <div className="flex flex-col text-4xl text-green-600">
+                            <h3>Uptime: </h3>
+                            <h3>Time Stamp: </h3>
+                            <h3>Database Size: </h3>
+                        </div>
+                        <div className="flex flex-col text-4xl text-green-600">
+                            <h3>{data.uptime}</h3>
+                            <h3>{data.timeStamp}</h3>
+                            <h3>{data.availability.database.size}</h3>
+                        </div>
+                    </div>
+                ) : (
+                    ""
+                )}
             </div>
         </>
     );
