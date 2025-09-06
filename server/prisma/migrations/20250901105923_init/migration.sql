4ed5a89d20f421f63aa7b3518a6e1ba69d3aa4f2
Md-Mahir-Asef <mdmahirasef2008@gmail.com>
2025-09-06 17:13:49 +0600

    Fix(DB): fix problem with prisma migration.

--- server/prisma/migrations/20250901105923_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "public"."Roles" AS ENUM ('user', 'admin', 'visitor');
-
--- AlterTable
-ALTER TABLE "public"."User" ADD COLUMN     "role" "public"."Roles" NOT NULL DEFAULT 'user';
