30775ec484b17c4a0f39a904bdc50a44b6a9d4f3
Md-Mahir-Asef <mdmahirasef2008@gmail.com>
2025-09-07 15:28:35 +0600

    Fix(Token-Issue): Fix can't get token issue on register or login.

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -30,7 +30,7 @@ export const register = async (req: Request, res: Response) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: false, // dev
-            sameSite: "none",
+            sameSite: "lax",
             path: "/",
             maxAge: 1000 * 60 * 60 * 24 * 7,
         });
