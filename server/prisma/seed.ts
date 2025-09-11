2efee30f6e73c6d1dc4232abb75e27600efa7fed
Md-Mahir-Asef <mdmahirasef2008@gmail.com>
2025-09-11 12:10:50 +0600

    Feat(Seed): Add a fake data seeder feature in the development that will seed fake data in every container spinup.

---
+++ server/prisma/seed.ts
@@ -0,0 +1,42 @@
+import prisma from "../src/utils/prisma";
+import { faker } from "@faker-js/faker";
+import { hasher } from "../src/utils/hasher";
+import logger from "../src/utils/logger";
+
+const NumOfUsers = 10;
+
+const seed = async () => {
+    try {
+        logger.info("Seeding the Database...");
+        await prisma.user.create({
+            data: {
+                user_name: "Md. Mahir Asef",
+                email: "mahir@asef.ecom",
+                password: "MahirCan'tPass420",
+                role: "admin",
+            },
+        });
+        await prisma.user.create({
+            data: {
+                user_name: "admin",
+                email: "admin@admin.ecom",
+                password: hasher("pass@123"),
+                role: "visitor",
+            },
+        });
+        for (let i = 0; i < NumOfUsers; i++) {
+            await prisma.user.create({
+                data: {
+                    user_name: faker.person.fullName(),
+                    email: faker.internet.email(),
+                    password: faker.internet.password(),
+                },
+            });
+        }
+        logger.info("Seeding Finished.");
+    } catch (err) {
+        logger.error(`Can't seed due to Error: \n${err}`);
+    }
+};
+
+seed();
